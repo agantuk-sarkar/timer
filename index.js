b1bc22ab8f6fce92d7d92d234be6245e53fae2a4
Agantuk Sarkar <agantuksarkar124@gmail.com>
2024-03-12 13:18:14 +0530

    Updated timer start

--- index.js
+++ index.js
@@ -5,5 +5,26 @@ let start_button = document.querySelector(".buttons>button:nth-child(1)");
 let reset_button = document.querySelector(".buttons>button:nth-child(2)");
 
 // Click event for start and stop buttons
-start_button.addEventListener("click",handleTimerStart);
-reset_button.addEventListener("click",handleTimerReset);
+start_button.addEventListener("click", handleTimerStart);
+reset_button.addEventListener("click", handleTimerReset);
+
+// Interval id
+let intervalId = null;
+let seconds = 0;
+let minutes = 0;
+
+// function to handle timer start button
+function handleTimerStart() {
+  intervalId = setInterval(function () {
+    seconds++;
+
+    if (seconds === 59) {
+      minutes++;
+      seconds = 0;
+    }
+    console.log(seconds);
+
+    timer_second.textContent = seconds;
+    timer_minute.textContent = minutes;
+  }, 200);
+}
