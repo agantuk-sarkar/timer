f725ff7d563508bd1b16d6949deea9d5c5613f1a
Agantuk Sarkar <agantuksarkar124@gmail.com>
2024-03-14 00:43:22 +0530

    Updated timer reset

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ let timer_second = document.querySelector(".timer-second");
 let start_button = document.querySelector(".buttons>button:nth-child(1)");
 let reset_button = document.querySelector(".buttons>button:nth-child(2)");
 
-// Click event for start and stop buttons
+// Click event for start and reset buttons
 start_button.addEventListener("click", handleTimerStart);
-// reset_button.addEventListener("click", handleTimerReset);
+reset_button.addEventListener("click", handleTimerReset);
 
 // Interval id for handling seconds and minutes in a timer
 let intervalId = null;
@@ -15,10 +15,10 @@ let intervalId = null;
 let seconds = 0;
 let minutes = 0;
 
-// flag is used to check if the condition is true then change the button name from start to stop
+// flag is used to check if the condition is true then change the button value from start to stop
 let flag = false;
 
-// temp is used to check if the condition is true then change the button name from stop to start
+// temp is used to check if the condition is true then change the button value from stop to start
 let temp = false;
 
 // function to handle timer start button
@@ -35,15 +35,16 @@ function handleTimerStart() {
 
     start_button.removeEventListener("click", handleTimerStart);
 
+    // on single click both the event is triggered at the same moment
     start_button.addEventListener("click", handleTimerStop);
-    start_button.addEventListener("click",function(){
+    start_button.addEventListener("click", function () {
       clearInterval(intervalId);
       if (temp === true) {
         start_button.textContent = "START";
       }
-      start_button.removeEventListener("click",handleTimerStop);
-      start_button.addEventListener("click",handleTimerStart);
-    })
+      start_button.removeEventListener("click", handleTimerStop);
+      start_button.addEventListener("click", handleTimerStart);
+    });
 
     if (seconds === 59) {
       minutes++;
@@ -59,4 +60,16 @@ function handleTimerStart() {
 // function to handle timer stop button
 function handleTimerStop() {
   clearInterval(intervalId);
+}
+
+// function to handle timer reset button
+function handleTimerReset() {
+  clearInterval(intervalId);
+  start_button.removeEventListener("click", handleTimerStop);
+  start_button.addEventListener("click", handleTimerStart);
+  seconds = 0;
+  minutes = 0;
+  timer_second.textContent = seconds;
+  timer_minute.textContent = minutes;
+
 }
